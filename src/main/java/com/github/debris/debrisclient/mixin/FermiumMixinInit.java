ae89b120a09b4c1cc0833e99b8a7b56fc0982bdc
De6ris <2838369930@qq.com>
2025-10-24 21:28:26 +0800

    set fermium to compile on github

--- src/main/java/com/github/debris/debrisclient/mixin/FermiumMixinInit.java
+++ src/main/java/com/github/debris/debrisclient/mixin/FermiumMixinInit.java
@@ -1,19 +1,22 @@
 package com.github.debris.debrisclient.mixin;
 
 import com.github.debris.debrisclient.DebrisClient;
+import fermiumbooter.FermiumRegistryAPI;
 import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
-import zone.rong.mixinbooter.IEarlyMixinLoader;
+import org.spongepowered.asm.launch.MixinBootstrap;
 
 import javax.annotation.Nullable;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 @IFMLLoadingPlugin.Name(DebrisClient.MOD_ID)
 @IFMLLoadingPlugin.MCVersion(ForgeVersion.mcVersion)
-public class MixinInit implements IFMLLoadingPlugin, IEarlyMixinLoader {
-    public MixinInit() {
+public class FermiumMixinInit implements IFMLLoadingPlugin {
+    public FermiumMixinInit() {
+        MixinBootstrap.init();
+
+        FermiumRegistryAPI.enqueueMixin(false, "mixins.debrisclient.json");
+        FermiumRegistryAPI.enqueueMixin(true, "mixins.debrisclient_late.json");
     }
 
     @Override
@@ -39,10 +42,5 @@ public class MixinInit implements IFMLLoadingPlugin, IEarlyMixinLoader {
     @Override
     public String getAccessTransformerClass() {
         return null;
-    }
-
-    @Override
-    public List<String> getMixinConfigs() {
-        return Collections.singletonList("mixins.debrisclient.json");
     }
 }
